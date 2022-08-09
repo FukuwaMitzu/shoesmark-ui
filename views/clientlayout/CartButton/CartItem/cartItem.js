ae1dd2fa7fab70d6753f7d7c14778fec32f7d07a
FightingSenior <nhomltmt1@gmail.com>
2022-08-09 23:43:54 +0700

    cập nhật cartitem

---
+++ views/clientlayout/CartButton/CartItem/cartItem.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { TextField } from "@mui/material";
+
+export default function CartItem(data) {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Image width={"100%"} height={"75px"} src={`${data.image}`} />
+      <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography width={'250px'} noWrap="true">
+            Chuck 70 Canvas
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
+            Trắng
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: "flex", alignItems: "center", pl: 2, pb: 1, gap: 1}}>
+          <Typography>Size:40</Typography>
+          <TextField label="Số lượng" type={'number'} size="small" sx={{width:"12ch"}}/>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography>2.999.000 đ</Typography>
+        </CardContent>
+        <Box sx={{ display: "flex", alignItems: "center", pl: 9, pb: 1 }}>
+          <IconButton aria-label="delete">
+            <DeleteIcon color="error" />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- views/clientlayout/CartButton/cartButton.tsx
+++ views/clientlayout/CartButton/cartButton.tsx
@@ -4,7 +4,11 @@ import Badge from "@mui/material/Badge";
 import Popover from "@mui/material/Popover";
 import { useState } from "react";
 import Box from "@mui/material/Box";
-
+import CartItem from "./CartItem/cartItem";
+import Button from "@mui/material/Button";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import Stack from "@mui/material/Stack";
+import { Divider } from "@mui/material";
 interface CartButtonProps {
 
 }
@@ -38,6 +42,31 @@ const CartButton: React.FC<CartButtonProps> = (data) => {
                 }}
                 onClose={handleClose}
             >
+            {//TODO: Thêm giao diện giỏ hàng
+            }
+            <Box sx={{ width: "100%" }}>
+            <Stack direction="column" divider={<Divider></Divider>}>
+                <CartItem image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2f3b27f9624241c3a13cac0000cb69ba_9366/TOP_TEN_HI_STAR_WARS_mau_xanh_la_FZ3465_01_standard.jpg">
+                
+                </CartItem>
+
+                <CartItem image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2f3b27f9624241c3a13cac0000cb69ba_9366/TOP_TEN_HI_STAR_WARS_mau_xanh_la_FZ3465_01_standard.jpg">
+                
+                </CartItem>
+                <CartItem image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2f3b27f9624241c3a13cac0000cb69ba_9366/TOP_TEN_HI_STAR_WARS_mau_xanh_la_FZ3465_01_standard.jpg">
+                
+                </CartItem>
+                
+                
+            </Stack>
+            </Box>
+           
+            <Box sx={{display:"flex", flexDirection:"row-reverse", position:"sticky", bottom: 0, backgroundColor:"background.paper", paddingY: 1}}>
+                <Button variant="text">
+                    Xem tất cả
+                    <ArrowRightIcon fontSize="large"/>
+                </Button>
+            </Box>
             </Popover>
         </Box>
     )
