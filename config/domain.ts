f000919dd115736ef281a105f7c9568fd1f03db0
FukuwaMitzu <an01647719638@gmail.com>
2022-08-25 00:17:30 +0700

    Cập nhật

--- config/domain.ts
+++ config/domain.ts
@@ -1,2 +1,2 @@
-// export const SHOESMARK_API_DOMAIN = "https://shoesmark.herokuapp.com";
-export const SHOESMARK_API_DOMAIN = "http://localhost:3300";
+export const SHOESMARK_API_DOMAIN = "https://shoesmark.herokuapp.com";
+// export const SHOESMARK_API_DOMAIN = "http://localhost:3300";
