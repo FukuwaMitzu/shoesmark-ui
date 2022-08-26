0ce21bf904a1f3878367618a4119c2941da30dfe
FukuwaMitzu <an01647719638@gmail.com>
2022-08-26 12:46:23 +0700

    Cap nhat

--- config/domain.ts
+++ config/domain.ts
@@ -1,2 +1,2 @@
-export const SHOESMARK_API_DOMAIN = "https://shoesmark.herokuapp.com";
+export const SHOESMARK_API_DOMAIN = "https://shoesmark-api-prod.herokuapp.com";
 // export const SHOESMARK_API_DOMAIN = "http://localhost:3300";
